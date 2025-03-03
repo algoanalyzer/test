c12b88e0ab96051b00cbf0b60bd552f51bd62855
algoanalyzer <algoanalyzer1337@gmail.com>
2025-03-03 10:32:31 +0530

    Update bsbutton.tsx

---
+++ app/_components/_buttons/bsbutton.tsx
@@ -0,0 +1,70 @@
+"use client" // Client-side Component to allow for routing.
+
+import { useRouter } from 'next/navigation'
+import { MouseEvent, useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks'
+import { selectRollNumber, updateRollNumber } from '@/lib/features/userData/userDataSlice'
+
+/**
+ * Function to create/fetch userID and proceed with getAlgorithm API.
+ * @param e (optional) HTML element whose properties can be modified.
+ * @param router Next.js router object.
+ * @param backend Boolean to indicate usage of backend.
+ * @param setRollNumber Function to store userId.
+ * @param route Page to redirect to.
+ */
+const onClickAgree = async (
+  e: MouseEvent<HTMLButtonElement>,
+  router: ReturnType<typeof useRouter>,
+  backend: boolean,
+  setRollNumber: Function
+) => {
+  e.currentTarget.disabled = true;
+
+  if (backend) {
+    let input: string | null = "";
+    while (input === "" || input === null) {
+      input = prompt("Please enter your roll number.");
+      if (input !== null) {
+        setRollNumber(input);
+        localStorage.setItem("rollNumber", input);
+        console.log("Navigating to: /level-zero-bs");
+        router.replace("/level-zero-bs"); // Hardcoded route
+      } else {
+        alert("Please enter your roll number to proceed further!");
+      }
+    }
+  } else {
+    setRollNumber("testRollNumber");
+    console.log("Navigating to: /level-zero-bs");
+    router.push("/level-zero-bs");
+
+  }
+};
+
+
+
+/**
+ * Function to generate an Agree button.
+ * @param route Page to redirect to.
+ * @returns Button which carries out the Agree function.
+ */
+
+function BSButton({ route, start }: { route: string; start?: boolean }) {
+  const router = useRouter();
+
+  return (
+    <button
+      type="button"
+      className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-4 m-1 rounded text-lg"
+      onClick={() => router.replace(route)}
+    >
+      {start ? "Start" : "Bubble Sort"}
+    </button>
+  );
+}
+
+
+
+
+export default BSButton
